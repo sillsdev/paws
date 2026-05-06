4dc5f79d822a6ea18051624e1d2fabd718422adc
Andy Black <blackhandrew@gmail.com>
2026-05-06 11:33:08 -0700

    Version 1.6.2

--- src/org/sil/paws/Constants.java
+++ src/org/sil/paws/Constants.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016-2024 SIL International
+ * Copyright (c) 2016-2026 SIL International
  * This software is licensed under the LGPL, version 2.1 or later
  * (http://www.gnu.org/licenses/lgpl-2.1.html)
  */
@@ -10,7 +10,7 @@ package org.sil.paws;
  *
  */
 public class Constants {
-	public static final String VERSION_NUMBER = "1.6.1";
+	public static final String VERSION_NUMBER = "1.6.2";
 	public static final String CURRENT_DATABASE_VERSION = "15";
 	public static final long OUTPUT_TRANSFORM_COUNT = 17;
 
